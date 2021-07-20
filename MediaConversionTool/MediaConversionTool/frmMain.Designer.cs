35db524485d253590238c088010466feb19aaed2
Alan Richards <87658732+AeonAKF@users.noreply.github.com>
2021-07-20 13:03:07 +0100

    Add files via upload

---
+++ MediaConversionTool/MediaConversionTool/frmMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace MediaConversionTool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtBoxOpen = new System.Windows.Forms.TextBox();
+            this.txtBoxSave = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.txtBoxFileName = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.prgConvert = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(9, 10);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(392, 54);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "1 - Open Media File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(9, 102);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(392, 57);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "2 - Save to Folder";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(9, 266);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(286, 41);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "4 - Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(299, 266);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(102, 41);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtBoxOpen
+            // 
+            this.txtBoxOpen.Location = new System.Drawing.Point(9, 69);
+            this.txtBoxOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBoxOpen.Name = "txtBoxOpen";
+            this.txtBoxOpen.Size = new System.Drawing.Size(393, 20);
+            this.txtBoxOpen.TabIndex = 4;
+            this.txtBoxOpen.Text = "D:\\Temp\\File.mov";
+            // 
+            // txtBoxSave
+            // 
+            this.txtBoxSave.Location = new System.Drawing.Point(9, 163);
+            this.txtBoxSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBoxSave.Name = "txtBoxSave";
+            this.txtBoxSave.Size = new System.Drawing.Size(393, 20);
+            this.txtBoxSave.TabIndex = 5;
+            this.txtBoxSave.Text = "D:\\Temp\\";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(9, 193);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(69, 13);
+            this.lblFileName.TabIndex = 6;
+            this.lblFileName.Text = "3 - File Name";
+            // 
+            // txtBoxFileName
+            // 
+            this.txtBoxFileName.Location = new System.Drawing.Point(82, 193);
+            this.txtBoxFileName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBoxFileName.Name = "txtBoxFileName";
+            this.txtBoxFileName.Size = new System.Drawing.Size(320, 20);
+            this.txtBoxFileName.TabIndex = 7;
+            this.txtBoxFileName.Text = "Output.mp4";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(14, 218);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(397, 17);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "A message will be displayed when the conversion is complete.";
+            // 
+            // prgConvert
+            // 
+            this.prgConvert.Location = new System.Drawing.Point(9, 238);
+            this.prgConvert.MarqueeAnimationSpeed = 0;
+            this.prgConvert.Name = "prgConvert";
+            this.prgConvert.Size = new System.Drawing.Size(391, 23);
+            this.prgConvert.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.prgConvert.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 312);
+            this.Controls.Add(this.prgConvert);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtBoxFileName);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.txtBoxSave);
+            this.Controls.Add(this.txtBoxOpen);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Media Conversion Tool v1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtBoxOpen;
+        private System.Windows.Forms.TextBox txtBoxSave;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox txtBoxFileName;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.ProgressBar prgConvert;
+    }
+}
+
---
+++ MediaConversionTool/MediaConversionTool/frmMain.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MediaConversionTool
+{
+    public partial class frmMain : Form
+    {
+        // Select output folder
+        private FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+        // Delegate for safe cross thread access from conversion thread to main window thead
+        private delegate void SafeCallDelegate(bool blEnabled);
+        private delegate void SafeCallDelegate1(int intZero);
+        private delegate void SafeCallDelegate2(ProgressBarStyle cont);
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "mov files (*.mov)|*.mov|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    txtBoxOpen.Text = openFileDialog1.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error Opening File. Message - " + ex.Message);
+                }
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtBoxSave.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // Disable controls to avoid multiple attempts at converting th same file
+            btnConvert.Enabled = false;
+            btnExit.Enabled = false;
+            btnOpen.Enabled = false;
+            btnSave.Enabled = false;
+            txtBoxFileName.Enabled = false;
+            txtBoxOpen.Enabled = false;
+            txtBoxSave.Enabled = false;
+
+            prgConvert.Style = ProgressBarStyle.Marquee;
+            prgConvert.MarqueeAnimationSpeed = 30;
+
+            // Create media conversion thread
+            Thread th = new Thread(new ThreadStart(convert));
+
+            // Start media conversion thread
+            try
+            {
+                th.Start();
+            }
+            catch (Exception ex)
+            {
+                prgConvert.MarqueeAnimationSpeed = 0;
+                prgConvert.Style = ProgressBarStyle.Continuous;
+                btnConvert.Enabled = true;
+                btnExit.Enabled = true;
+                btnOpen.Enabled = true;
+                btnSave.Enabled = true;
+                txtBoxFileName.Enabled = true;
+                txtBoxOpen.Enabled = true;
+                txtBoxSave.Enabled = true;
+
+                if (th.ThreadState != ThreadState.Aborted) { th.Abort(); }
+
+                MessageBox.Show("Error converting file: " + ex.Message);
+            }
+
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program uses the MediaToolKit by Aydin Adn. I used it simply to convert MOV files to MP4, it is capable of a lot more but I have not tried anything else. Feel free to modify this program to suit your needs and explore the full functionality of the kit. \r\n\r\n More info can be found at https://github.com/AydinAdn/MediaToolkit", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Convert MOV file to MP4
+        private void convert()
+        {
+            var inputFile = new MediaToolkit.Model.MediaFile { Filename = txtBoxOpen.Text };
+            var outputFile = new MediaToolkit.Model.MediaFile { Filename = txtBoxSave.Text + "\\" + txtBoxFileName.Text };
+
+            using (var engine = new MediaToolkit.Engine())
+            {
+                engine.Convert(inputFile, outputFile);
+            }
+
+            // Safely enable controls
+            SetEnabled();
+
+            MessageBox.Show("Done", "Conversion Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Check need for invocation of controls from conversion thread
+        private void SetEnabledSafe(bool blEnabled1)
+        {
+            if (btnConvert.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                btnConvert.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                btnConvert.Enabled = blEnabled1;
+            }
+
+            if (btnExit.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                btnExit.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                btnExit.Enabled = blEnabled1;
+            }
+
+            if (btnOpen.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                btnOpen.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                btnOpen.Enabled = blEnabled1;
+            }
+
+            if (btnSave.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                btnSave.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                btnSave.Enabled = blEnabled1;
+            }
+
+            if (txtBoxFileName.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                txtBoxFileName.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                txtBoxFileName.Enabled = blEnabled1;
+            }
+
+            if (txtBoxOpen.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                txtBoxOpen.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                txtBoxOpen.Enabled = blEnabled1;
+            }
+
+            if (txtBoxSave.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                txtBoxSave.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                txtBoxSave.Enabled = blEnabled1;
+            }
+
+            if (prgConvert.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                prgConvert.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                prgConvert.MarqueeAnimationSpeed = 0;
+            }
+
+            if (prgConvert.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetEnabledSafe);
+                prgConvert.Invoke(d, new object[] { blEnabled1 });
+            }
+            else
+            {
+                prgConvert.Style = ProgressBarStyle.Continuous;
+            }
+        }
+
+        private void SetEnabledSafe1(int int1)
+        {
+
+            if (prgConvert.InvokeRequired)
+            {
+                var d = new SafeCallDelegate1(SetEnabledSafe1);
+                prgConvert.Invoke(d, new object[] { int1 });
+            }
+            else
+            {
+                prgConvert.MarqueeAnimationSpeed = int1;
+            }
+        }
+
+        private void SetEnabledSafe2(ProgressBarStyle prgCont)
+        {
+
+            if (prgConvert.InvokeRequired)
+            {
+                var d = new SafeCallDelegate2(SetEnabledSafe2);
+                prgConvert.Invoke(d, new object[] { prgCont });
+            }
+            else
+            {
+                prgConvert.Style = prgCont;
+            }
+
+        }
+
+        private void SetEnabled()
+        {
+            SetEnabledSafe(true);
+            SetEnabledSafe1(0);
+            SetEnabledSafe2(ProgressBarStyle.Continuous);
+        }
+    }
+}
